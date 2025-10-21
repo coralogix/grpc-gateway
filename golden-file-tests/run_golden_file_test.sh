0ec676c9546f013a0a57b90f99d112eddfc7dd5e
Luigi Taglialatela <luigi.taglialatela1996@gmail.com>
2025-10-21 08:14:09 +0200

    Refactor oneOf variants in schema components

--- golden-file-tests/run_golden_file_test.sh
+++ golden-file-tests/run_golden_file_test.sh
@@ -2,10 +2,24 @@
 cd cx-api && bash build_facade_files.sh
 cd ..
 
-if diff <(jq --sort-keys . apidocs_golden.swagger.json) <(jq --sort-keys . apidocs.swagger.json) > /dev/null; then
-  echo "Plugin output matches golden file."
+if diff <(jq --sort-keys . apidocs_golden.swagger.json | jq 'walk(if type == "object" and has("oneOf") and (.oneOf | type) == "array" then .oneOf |= sort_by(."$ref") else . end)') \
+     <(jq --sort-keys . apidocs.swagger.json | jq 'walk(if type == "object" and has("oneOf") and (.oneOf | type) == "array" then .oneOf |= sort_by(."$ref") else . end)') > /dev/null; then
+  echo "Plugin output matches golden file without preview visibility."
 else
-  echo "Plugin output does not match golden file."
+  echo "Plugin output does not match golden file without preview visibility."
+  exit 1
+fi
+
+rm -r apidocs.swagger.json
+
+cd cx-api && bash build_facade_files_with_preview_visibility.sh
+cd ..
+
+if diff <(jq --sort-keys . apidocs_golden_preview_visibility.swagger.json | jq 'walk(if type == "object" and has("oneOf") and (.oneOf | type) == "array" then .oneOf |= sort_by(."$ref") else . end)') \
+     <(jq --sort-keys . apidocs.swagger.json | jq 'walk(if type == "object" and has("oneOf") and (.oneOf | type) == "array" then .oneOf |= sort_by(."$ref") else . end)') > /dev/null; then
+  echo "Plugin output matches golden file with preview visibility."
+else
+  echo "Plugin output does not match golden file with preview visibility."
   exit 1
 fi
 
--- protoc-gen-openapiv3/internal/genopenapi/helpers.go
+++ protoc-gen-openapiv3/internal/genopenapi/helpers.go
@@ -3,6 +3,11 @@
 
 package genopenapi
 
+import (
+	"strings"
+	"unicode"
+)
+
 // this method will filter the same fields and return the unique one
 func getUniqueFields(schemaFieldsRequired []string, fieldsRequired []string) []string {
 	var unique []string
@@ -22,4 +27,29 @@ func getUniqueFields(schemaFieldsRequired []string, fieldsRequired []string) []s
 		}
 	}
 	return unique
+}
+
+func toPascalCase(s string) string {
+	if s == "" {
+		return ""
+	}
+
+	var builder strings.Builder
+	capitalizeNext := true
+
+	for _, r := range s {
+		if r == '_' {
+			capitalizeNext = true
+			continue // Skip the underscore itself
+		}
+
+		if capitalizeNext {
+			builder.WriteRune(unicode.ToUpper(r))
+			capitalizeNext = false // Reset the flag
+		} else {
+			builder.WriteRune(r)
+		}
+	}
+
+	return builder.String()
 }
